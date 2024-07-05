20765f1ee22cde333c1ca7210a878fe1793a4cf8
Johan Hanses <johanhanses@gmail.com>
2024-07-05 17:48:25 +0200

    add some links

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Root } from "./routes/root";
-import { Analytics } from "@vercel/analytics/react"
-import "./style.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Root } from './routes/root'
+import { Analytics } from '@vercel/analytics/react'
+import './style.css'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
   },
-]);
+])
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Analytics />
     <RouterProvider router={router} />
   </React.StrictMode>,
-);
+)
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom'
+
 export function Root() {
   return (
-    <div className="grid h-screen items-center">
-      <h1 className="text-3xl text-center font-serif">Johan Hanses</h1>
+    <div className="grid h-screen">
+      <div className="grid gap-4 place-self-center">
+        <h1 className="mb-2 text-center font-serif text-3xl">Johan Hanses</h1>
+        <div className="flex flex-col gap-2">
+          <Link to="https://github.com/johanhanses" target="_blank" className="hover:text-blue-500 hover:underline">
+            GitHub
+          </Link>
+          <Link
+            to="https://www.linkedin.com/in/johanhanses/"
+            target="_blank"
+            className="hover:text-blue-500 hover:underline"
+          >
+            LinkedIn
+          </Link>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
